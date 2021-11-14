243c5dfe27ed5360f5e8ffc2e4e964a39d3292d6
ak7362 <91259337+ak7362@users.noreply.github.com>
2021-11-14 09:00:38 +0530

    Delete Factorial.java

--- Factorial.java
+++
@@ -1,9 +0,0 @@
-public class Factorial {
-    public static void main(String args[]){
-        int fact=1,i;
-        for(i=5;i>=1;i--){
-            fact=fact*i;
-        }
-        System.out.println(fact);
-    }
-}

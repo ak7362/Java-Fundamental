6ebf31a4d80726dc785620b3a101454c76b4fcb4
ak7362 <91259337+ak7362@users.noreply.github.com>
2021-11-15 16:59:16 +0530

    Add files via upload

---
+++ Ascii.java
@@ -0,0 +1,16 @@
+package Assignment;
+import java.util.Scanner;
+public class Ascii {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+     System.out.println("Enter the first Character:");
+     char a=sc.next().charAt(0);
+     System.out.println("Enter the first Character:");
+     char b=sc.next().charAt(0);
+     System.out.println("Enter the first Character:");
+     char c=sc.next().charAt(0);
+     int d=a+b+c;
+     System.out.printf("Sum of ASCII Value is %d where equivalent character is %c",d,d);
+        sc.close();
+    }  
+}

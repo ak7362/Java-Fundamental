abd25d2d7700fbae1b874342709a5f5a94e6d7f4
ak7362 <91259337+ak7362@users.noreply.github.com>
2021-11-18 10:41:18 +0530

    Add files via upload

---
+++ Print1_TO_10.java
@@ -0,0 +1,10 @@
+public class Print1_TO_10 {
+    public static void main(String args[]) {
+        int i = 1;
+        while (i <= 10) {
+          System.out.print(i + " ");
+          i++;
+        }
+    }  
+
+}

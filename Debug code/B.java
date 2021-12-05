0fc59467090116ff1d73b5fd6076f238cfe35d4f
ak7362 <91259337+ak7362@users.noreply.github.com>
2021-12-05 08:39:21 +0530

    Add files via upload

---
+++ Debug code/B.java
@@ -0,0 +1,10 @@
+package Pack2;
+
+import pack1.*;
+
+class B {
+    public static void main(String args[]) {
+        A a = new A();  //yes it's compile time Error if class is not public
+
+    }
+}
